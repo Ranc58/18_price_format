d097dee68f971d8084cc31da5e3785efdd162a6c
Ranc58 <rancvova@gmail.com>
2017-06-21 14:04:07 +0300

    init tests.py init format_price.py

--- format_price.py
+++ format_price.py
@@ -1,5 +1,34 @@
-def format_price(price):
-    pass
+import argparse
+import locale
+
+
+def format_price(input_price):
+    locale.setlocale(locale.LC_ALL, '')
+    try:
+        if isinstance(input_price, (int, float)):
+            output_price = ("{:n}".format(round(input_price,2)))
+        else:
+            input_price = float(input_price.replace(',', '.'))
+            output_price = "{:n}".format(round(input_price,2))
+    except ValueError:
+        return None
+    else:
+        return output_price.replace(u'\xa0', u' ')
+
+
+def create_parser_for_user_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--price',
+                        help="set price")
+    options = parser.parse_args()
+    return options
+
 
 if __name__ == '__main__':
-    pass
+    options = create_parser_for_user_arguments()
+    input_price = options.price
+    output_price = format_price(input_price)
+    if output_price is not None:
+        print(output_price)
+    else:
+        print("Error! '{}'-incorrect value!".format(input_price))
--- tests.py
+++ tests.py
@@ -1,1 +1,32 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+
+class TestPriceConvert(unittest.TestCase):
+
+    def test_int(self):
+        formated_price = format_price(3213.00)
+        self.assertEqual(formated_price, '3 213')
+
+    def test_float(self):
+        formated_price = format_price(3213.54)
+        self.assertEqual(formated_price, '3 213,54')
+
+    def test_str(self):
+        formated_price = format_price('3213.54')
+        self.assertEqual(formated_price, '3 213,54')
+
+    def test_comma_separated_str(self):
+        formated_price = format_price('3213,54')
+        self.assertEqual(formated_price, '3 213,54')
+
+    def test_words_in_str(self):
+        formated_price = format_price('321fd3,54')
+        self.assertEqual(formated_price, None)
+
+    def test_big_number(self):
+        formated_price=format_price('3213.98344324')
+        self.assertEqual(formated_price,'3 213,98')
+
+if __name__ == '__main__':
+    unittest.main()
